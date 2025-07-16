056d45c5bd46898c93bd2f77cbbb16897fb9c879
Kaustubh Suryakant Deshpande <216578302+kaustubhsuryakantdeshpande@users.noreply.github.com>
2025-07-16 08:27:36 +0000

    Add Family Age Calculator program with member management features

--- HelloWorld/obj/Debug/net8.0/HelloWorld.AssemblyInfo.cs
+++ HelloWorld/obj/Debug/net8.0/HelloWorld.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+29fb1d591b1932225fb2d215b936a509c411e317")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5b62ee55941d1defad3f7419043338cd1db71198")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
