0880fa744fda61ab89fc4e977ee03b87e882801e
Kaustubh Suryakant Deshpande <216578302+kaustubhsuryakantdeshpande@users.noreply.github.com>
2025-07-23 08:26:01 +0000

    Update assembly information and documentation for HelloWorld project

--- HelloWorld/obj/Debug/net8.0/HelloWorld.AssemblyInfo.cs
+++ HelloWorld/obj/Debug/net8.0/HelloWorld.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c545916211367136747da04724283e46f6bb2645")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ae519925adb4c360143231f3e617ab8d406f40a0")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
